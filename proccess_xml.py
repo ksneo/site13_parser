66a31be84733b3dbb27eb18108d4432f6784c62c
ksneo <vkashapov@gmail.com>
2019-05-24 08:12:56 +0300

    Fix function get_offer

--- proccess_xml.py
+++ proccess_xml.py
@@ -1,6 +1,10 @@
 import xml.etree.ElementTree as ET
+import logging
 
-ROOT_FOLDER = '00000000-0000-0000-0000-000000000000'
+logger = logging.getLogger()
+
+EMPTY_UUID = '00000000-0000-0000-0000-000000000000'
+ROOT_FOLDER = EMPTY_UUID
 
 def get_text(el, default=None):
     if not el is None:
@@ -20,44 +24,80 @@ def is_folder(el):
 
     return False
 
+def get_volume(el, tree):
+    result = ""
+    result_name = ""
+    try:
+        volume_ref = get_text(el.find("./ЕдиницаХраненияОстатков"))
+        volume_el = tree.find(f".//CatalogObject.ЕдиницыИзмерения[Ref='{volume_ref}']")
+        result = get_text(volume_el.find("./Объем"),"")
+        volume_name_ref = get_text(volume_el.find("./ЕдиницаПоКлассификатору"), "")
+        volume_name_el = tree.find(f".//CatalogObject.КлассификаторЕдиницИзмерения[Ref='{volume_name_ref}']")
+        result_name = get_text(volume_name_el.find("./Description"))
+    except Exception as e:
+        logger.error(f"Ошибка получения единицы измерения {e}")
+
+    return (result, result_name)
+
 
 def get_offer(el, product_id, tree):
     offer = {}
     offer["product_id"] = product_id
-    product_ref = get_text(tree.find("./Номенклатура"))
-    product_el = tree.find(f".//CatalogObject.Номенклатура[Ref={product_ref}]")
-    offer["article"] = get_text(product_el.find("./Артикул"), '')
+    product_ref = get_text(el.find("./Номенклатура"))
+    product_el = tree.find(f".//CatalogObject.Номенклатура[Ref='{product_ref}']")
+    product_colour = get_text(el.find("./ХарактеристикаНоменклатуры"), "")
+    volume, volume_name = get_volume(product_el, tree)
+    extension_ref = get_text(el.find("./ДополнительнаяНоменклатура"))
+    extension_el = None
+
+    if not extension_ref == EMPTY_UUID:
+        extension_el = tree.find(f".//CatalogObject.Номенклатура[Ref='{extension_ref}']")
+
+    offer["article"] = "" if not product_el else get_text(product_el.find("./Артикул"), "")
+    offer["extension"] = "" if not extension_el else (get_text(product_el.find("./Code"), "")).strip()
+    offer["colour"] = get_text(el.find("./ХарактеристикаДополнительнойНоменклатуры"), "")
+    offer["colour"] = product_colour if not offer["colour"] else offer["colour"]
+    offer["volume"] = volume
+    offer["volume_name"] = volume_name
     return offer
 
 
 def read_product(el, tree):
     product = {}
+    product["type"] = "product"
     product["name"] = get_descr(el)
     product["product_id"] = get_text(el.find("./КодТовараНаСайте"))
     offer_elems = el.findall("./ТорговыеПредложения/Row")
-    product["offers"] = [get_offer(offer_elem, product["product_id"], tree) for offer_elem in offer_elems]
+    offers = []
+    for offer_elem in offer_elems:
+        offers.append(get_offer(offer_elem, product["product_id"], tree))
+    product["offers"] = offers
     return product
 
 
 def read_tree(el, tree):
-    parent_id = el.find("./Ref")
+    parent_id = get_text(el.find("./Ref"))
     if parent_id and is_folder(el):
         find_path = f".//CatalogObject.ТоварыНаСайте13[Parent='{parent_id}']"
         children = tree.findall(find_path)
+
         if len(children) == 0:
-            return {get_descr(el): {}}
+            return {"type": "folder", "name": get_descr(el), "children": []}
+
+        children_elems = []
         for child in children:
-            return {get_descr(el): read_tree(child, tree)}
+            children_elems.append(read_tree(child, tree))
+        return {"type": "folder", "name": get_descr(el), "children": children_elems}
     else:
         return read_product(el, tree)
 
 
 def proccess_xml(file):
-    result = {'sites': {}}
+    result = {"sites": []}
     xmlTree = ET.fromstring(file.read())
     root_xpath = f".//CatalogObject.ТоварыНаСайте13[Parent='{ROOT_FOLDER}']"
     root_elements = xmlTree.findall(root_xpath)
 
     for el in root_elements:
-        result["sites"][get_descr(el)] = read_tree(el, xmlTree)
+        result["sites"].append(read_tree(el, xmlTree))
     return result
--- tests/test_proccess_xml.py
+++ tests/test_proccess_xml.py
@@ -1,8 +1,10 @@
 import pytest
+import json
 from pytest_mock import mocker
 import xml.etree.ElementTree as ET
-from ..proccess_xml import proccess_xml, is_folder, get_descr
-from .fixtures.data_fixtures import category, root_folders
+from ..proccess_xml import (proccess_xml, is_folder,
+                            get_descr, read_tree, read_product)
+from .fixtures.data_fixtures import category, product, root_folders
 
 class TestProccessXml:
     def test_call_function_without_exceptions(self, mocker):
@@ -11,14 +13,26 @@ class TestProccessXml:
             proccess_xml(file)
         assert True
 
-    def test_real_file_open(self):
-        with open(".\\data\\data.xml", mode="r", encoding="utf-8") as file:
-            result = proccess_xml(file)
-        assert '' == result
-
     def test_isfolder(self, category):
         el = ET.fromstring(category)
         assert is_folder(el) == True
+
+    def test_get_descr(self):
+        xml_str = "<root><Description>test</Description></root>"
+        el = ET.fromstring(xml_str)
+        assert get_descr(el) == 'test'
+
+    def test_read_product(self, product, root_folders):
+        el = ET.fromstring(product)
+        tree = ET.fromstring(root_folders)
+        result = read_product(el, tree)
+        assert result == ''
+
+    def test_read_tree(self, category, root_folders):
+        el = ET.fromstring(category)
+        tree = ET.fromstring(root_folders)
+        result = read_tree(el, tree)
+        assert result == ''
 
     def test_proccess_xml_with_root(self, mocker, root_folders):
         mocker.patch("builtins.open", mocker.mock_open(read_data=root_folders))
@@ -26,7 +40,10 @@ class TestProccessXml:
             result = proccess_xml(file)
         assert result == ''
 
-    def test_get_descr(self):
-        xml_str = "<root><Description>test</Description></root>"
-        el = ET.fromstring(xml_str)
-        assert get_descr(el) == 'test'
+    def test_real_file_open(self):
+        with open(".\\data\\data.xml", mode="r", encoding="utf-8") as file:
+            result = proccess_xml(file)
+
+        with open(".\\output.json", mode="w", encoding="utf-8") as output:
+            json.dump(result, output, ensure_ascii=False, indent=4)
+        assert '' == result
